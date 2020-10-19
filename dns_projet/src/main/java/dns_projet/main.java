fb6e51c66e964b7d59f355ab312240265d0db5fc
olivier <olivier@localhost>
2020-10-19 14:46:17 +0200

    class websocket

--- dns_projet/src/main/java/dns_projet/main.java
+++ dns_projet/src/main/java/dns_projet/main.java
@@ -1,14 +1,19 @@
 package dns_projet;
 
 
-
+import java.io.IOException;
 import javax.naming.NamingException;
 
 import jndi.Dns;
-
+import websocket.Websocket; 
 public class main {
 
 	public static void main(String[] args) throws NamingException {
 		Dns _google = new Dns("samsung", ".fr");
+		try{
+			Websocket serveurWeb = new Websocket();
+		} catch(IOException e) {
+			System.out.println("error");
+		}
 	}
 }
