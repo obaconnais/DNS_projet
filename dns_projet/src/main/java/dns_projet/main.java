423a5f89b2ee02d57c8867d75a06e446ccdea5ed
olivier <olivier@localhost>
2020-10-19 21:48:45 +0200

    creation Websocket... j'ai recupérer les bonne bibliothèques pour org/glassfish

--- dns_projet/src/main/java/dns_projet/main.java
+++ dns_projet/src/main/java/dns_projet/main.java
@@ -3,6 +3,9 @@ package dns_projet;
 
 import java.io.IOException;
 import javax.naming.NamingException;
+
+import org.glassfish.tyrus.server.Server;
+
 import java.util.HashMap;
 
 import jndi.Dns;
@@ -15,6 +18,19 @@ public class main {
 		user_properties.put("co-Author1", "oBaconnais");
 		user_properties.put("co-Author2", "rBadanin");
 
-        org.glassfish.tyrus.server.Server server = new org.glassfish.tyrus.server.Server("localhost", 1963, "/oBaconnais ", user_properties /* or 'null' */, Websocket.class);
+        Server server = new Server("localhost", 1963, "/oBaconnais ", user_properties /* or 'null' */, Websocket.class);
+        try {
+        	server.start();
+            java.awt.Desktop.getDesktop().browse(java.nio.file.FileSystems.getDefault().getPath("web" + java.io.File.separatorChar + "index.html").toUri());
+            java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+            System.out.println("Please press a key to stop the server...");
+            reader.readLine();
+
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }
+        finally {
+        	server.stop();
+        }
 	}
 }
