5c2444f33d1c5c3a7b552d61edc192a1e0800f09
r0m1ntik <rom.badanin@gmail.com>
2020-11-09 11:52:07 +0100

    Test de repondes de js dans la console java

--- dns_projet/web/js/main.js
+++ dns_projet/web/js/main.js
@@ -5,18 +5,30 @@ window.onload = () => {
     let service = new WebSocket("ws://localhost:8025/oBaconnaisrBadanin/main");
     service.onmessage = (event) => {
         console.log("Message from Java: " + event.data);
+        service.send(
+                JSON.stringify(
+                {
+                    Response: "[JS]: La page Web est chargé chez le client."
+                })
+            );
     };
+    
     service.onopen = () => {
         console.log("service.onopen...");
         let response = window.confirm(service.url + " just opened... Say 'Hi!'?");
         if (response)
-            service.send(JSON.stringify({Response: "Hi!"}));
+            service.send(
+                JSON.stringify(
+                {
+                    Response: "[JS]: La page Web est ouverte chez le client."
+                })
+            );
     };
     service.onclose = (event/*:CloseEvent*/) => {
         console.log("service.onclose... " + event.code);
         window.alert("Bye! See you later...");
-// '1011': the server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.
     };
+    
     service.onerror = () => {
         window.alert("service.onerror...");
     };
